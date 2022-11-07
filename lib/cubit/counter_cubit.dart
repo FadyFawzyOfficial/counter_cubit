df79f85f52918de2c201f92a6653c1be6961103f
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-11-07 15:30:52 +0200

    5. Build Counter Cubit & its Action Functions

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,8 +1,22 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'counter_state.dart';
 
+//! Every Cubit is given a 'state' variable and an emit function that emits a new
+//! 'state' to a Stream.
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+  CounterCubit() : super(CounterState.initial());
+
+  void increment() {
+    final newState = state.copyWith(counter: state.counter + 1);
+    print(newState);
+    emit(newState);
+  }
+
+  void decrement() {
+    final newState = state.copyWith(counter: state.counter - 1);
+    print(newState);
+    emit(newState);
+  }
 }
